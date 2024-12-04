f44523320b45ab45df9b73dd36e4904463920f4d
Abhijeet18Ghode <ghodeabhijeet18@gmail.com>
2024-12-04 18:08:53 +0530

    fetch user data

--- src/app/api/getData/route.js
+++ src/app/api/getData/route.js
@@ -6,8 +6,11 @@ export async function GET(req) {
   const SHOPIFY_SHOP_NAME = '71c29e-3'; // Your Shopify store name
   const SHOPIFY_ACCESS_TOKEN = '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'; // Hardcoded access token
 
-  const url = `https://${SHOPIFY_SHOP_NAME}.myshopify.com/admin/api/2023-01/orders.json`;
-
+  const specificDate = "2024-12-04";
+//   const specificDate = "2024-12-01"; // Example date
+// const specificDate = "2024-12-01"; // Example date
+const url = `https://${SHOPIFY_SHOP_NAME}.myshopify.com/admin/api/2023-01/orders.json?created_at_min=${specificDate}T00:00:00&created_at_max=${specificDate}T23:59:59&status=any`;
+    
   try {
     const response = await axios.get(url, {
       headers: {
